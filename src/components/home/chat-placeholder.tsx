46b73b5bbbe08e4445ec9f5d5d18d0319afc8c9c
Roshini-24 <roshini.singh2402@gmail.com>
2024-05-23 15:50:17 +0530

    hero section updated

--- src/components/home/chat-placeholder.tsx
+++ src/components/home/chat-placeholder.tsx
@@ -1,23 +1,27 @@
-import { Lock } from "lucide-react";
+import { SquareArrowOutUpRight } from "lucide-react";
 import Image from "next/image";
 import { Button } from "../ui/button";
+import toast from "react-hot-toast";
 
 const ChatPlaceHolder = () => {
 	return (
 		<div className='w-3/4 bg-gray-secondary flex flex-col items-center justify-center py-10'>
 			<div className='flex flex-col items-center w-full justify-center py-10 gap-4'>
-				<Image src={"/desktop-hero.png"} alt='Hero' width={320} height={188} />
-				<p className='text-3xl font-extralight mt-5 mb-2'>Download WhatsApp for Windows</p>
+				<Image src={"/Right-image.png"} alt='Hero' width={320} height={188} />
+				<p className='text-3xl font-extralight mt-5 mb-2'>Chat with Individuals or Create Groups</p>
 				<p className='w-1/2 text-center text-gray-primary text-sm text-muted-foreground'>
-					Make calls, share your screen and get a faster experience when you download the Windows app.
+					Create Video-Call rooms, share your screen, share Videos & Images and many more.
 				</p>
 
 				<Button className='rounded-full my-5 bg-green-primary hover:bg-green-secondary'>
-					Get from Microsoft Store
+					Welcome To CHAT-APP
 				</Button>
 			</div>
-			<p className='w-1/2 mt-auto text-center text-gray-primary text-xs text-muted-foreground flex items-center justify-center gap-1'>
-				<Lock size={10} /> Your personal messages are end-to-end encrypted
+			<p className='w-1/2 mt-auto text-center text-gray-primary text-xs text-muted-foreground flex items-center justify-center gap-1 hover:cursor-pointer'
+			onClick={()=> {navigator.clipboard.writeText('https://chat-app-sepia-nine.vercel.app/');
+				toast.success("Link Copied Succesfully")
+			}}>
+				<SquareArrowOutUpRight size={10} /> Share with friends and families
 			</p>
 		</div>
 	);
