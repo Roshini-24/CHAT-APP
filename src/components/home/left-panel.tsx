bac0c36c4bd49b5b54e2ee889dba14e3ad4933df
Roshini-24 <roshini.singh2402@gmail.com>
2024-05-23 16:29:34 +0530

    loader added

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -1,6 +1,5 @@
 "use client"
-import { ListFilter,  Search } from "lucide-react";
-import { Input } from "../ui/input";
+
 import ThemeSwitch from "./theme-switch";
 
 import Conversation from "./conversation";
@@ -38,21 +37,7 @@ const LeftPanel = () => {
 						
 					</div>
 				</div>
-				<div className='p-3 flex items-center'>
-					{/* Search */}
-					<div className='relative h-10 mx-3 flex-1'>
-						<Search
-							className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10'
-							size={18}
-						/>
-						<Input
-							type='text'
-							placeholder='Search or start a new chat'
-							className='pl-10 py-2 text-sm w-full rounded shadow-sm bg-gray-primary focus-visible:ring-transparent'
-						/>
-					</div>
-					<ListFilter className='cursor-pointer' />
-				</div>
+				
 			</div>
 
 			{/* Chat List */}
